43e2838f255107921a757c9714918616438d883d
elsali123 <112128312+elsali123@users.noreply.github.com>
2025-06-09 11:32:09 -0700

    added sentence reranker

--- groq_faiss_RAG/main.py
+++ groq_faiss_RAG/main.py
@@ -4,7 +4,9 @@ from groq import Groq
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 import os
-
+from sentence_transformers import SentenceTransformer
+from nltk.tokenize import sent_tokenize
+from sklearn.metrics.pairwise import cosine_similarity
 
 def get_relevant_excerpts(user_question, docsearch):
     """
@@ -20,6 +22,51 @@ def get_relevant_excerpts(user_question, docsearch):
 
     return "\n\n---\n\n".join(excerpts)
 
+def chunk_document(text):
+    """
+    Chunk documents by using basic tokenizer
+    """
+    sentences = sent_tokenize(text)
+    return sentences
+
+def embed_texts(texts, model):
+    """
+    Embedding text, given model
+    """
+    embeddings = model.encode(texts, convert_to_tensor=True)
+    return embeddings
+
+def rerank_chunks(chunks, query, embed_model):
+    """
+    Ranking relevance of each chunk to query, returns a list of most to least relevant chunk along
+    with the relevance score
+    """
+    query_embedding = embed_model.encode(query)
+
+    chunk_embeddings = embed_model.encode(chunks)
+
+    similarities = cosine_similarity(
+        [query_embedding],
+        chunk_embeddings
+    )[0]
+
+    scored_chunks = list(zip(chunks, similarities))
+    scored_chunks.sort(key=lambda x: x[1], reverse=True)
+
+    return scored_chunks
+
+def rerank_context(excerpts, user_question, relevant_count, embed_model):
+    """
+    Performs reranking given excerpts, and returns a cleaned string with relevant_count
+    number of sentences pulled from excerpts
+    """
+    chunks = chunk_document(excerpts)
+
+    reranked = rerank_chunks(chunks, user_question, embed_model)
+
+    context = "\n".join([chunk for chunk, _ in reranked[:relevant_count]])
+
+    return context
 
 def generate_response(client, model, user_question, relevant_excerpts):
     """
@@ -68,8 +115,13 @@ def main():
             continue
 
         excerpts = get_relevant_excerpts(user_question, faiss_index)
-        response = generate_response(client, model, user_question, excerpts)
+
+        embed_model = SentenceTransformer("sentence-transformers/multi-qa-mpnet-base-dot-v1")
+        context = rerank_context(excerpts, user_question, 20, embed_model) # pulls the 20 most relevant sentences
+
+        response = generate_response(client, model, user_question, context)
         print("\n🧠 Answer:\n" + response + "\n")
+
 
 
 if __name__ == "__main__":
